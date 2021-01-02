77753f5d7950728386dad38004273d34ca237515
Minoru Sakamoto <p2020640@gmail.com>
2021-01-02 17:33:59 +0900

    s3を暗号化

--- tf/main.tf
+++ tf/main.tf
@@ -37,6 +37,14 @@ resource "aws_s3_bucket" "sakamoto-ninja-site" {
   acl           = "private"
   bucket        = "sakamoto-ninja-site"
   force_destroy = false
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = {
     "Service" = "sakamoto-ninja"
   }
